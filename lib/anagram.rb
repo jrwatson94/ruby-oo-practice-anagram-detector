8882fa82ff3a75d79033d19ed51f035255b044ae
jrwatson94 <j.watsonreid@gmail.com>
2020-09-20 14:47:27 -0400

    all tests passing

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+require "pry"
+
+
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(anagram_list)
+        anagrams = []
+        anagram_list.each do |item|
+            if @word.split("").sort == item.split("").sort
+                anagrams << item
+            end
+        end
+        anagrams
+    end
+end
